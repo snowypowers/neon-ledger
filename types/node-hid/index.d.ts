ebc43b6b46841b4681e6bd72b7a91fcd6e9c9e9b
Yak Jun Xiang <snowypowers@gmail.com>
2018-04-22 02:12:44 -0700

    re-declare node-hid

--- types/node-hid/index.d.ts
+++ types/node-hid/index.d.ts
@@ -1,30 +1,32 @@
-export interface Device {
-  vendorId: number;
-  productId: number;
-  path?: string;
-  serialNumber?: string;
-  manufacturer?: string;
-  product?: string;
-  release: number;
-  interface: number;
-  usagePage?: number;
-  usage?: number;
-}
+declare module "node-hid" {
+  export interface Device {
+    vendorId: number;
+    productId: number;
+    path?: string;
+    serialNumber?: string;
+    manufacturer?: string;
+    product?: string;
+    release: number;
+    interface: number;
+    usagePage?: number;
+    usage?: number;
+  }
 
-export class HID {
-  constructor(path: string);
-  constructor(vid: number, pid: number);
-  close(): void;
-  pause(): void;
-  read(callback: (err: any, data: number[]) => void): void;
-  readSync(): number[];
-  readTimeout(time_out: number): number[];
-  sendFeatureReport(data: number[]): number;
-  getFeatureReport(report_id: number, report_length: number): number[];
-  getDeviceInfo(): string;
-  resume(): void;
-  on(event: string, handler: (value: any) => void): void;
-  write(values: number[]): number;
-  setDriverType(type: string): void;
+  export class HID {
+    constructor(path: string);
+    constructor(vid: number, pid: number);
+    close(): void;
+    pause(): void;
+    read(callback: (err: any, data: number[]) => void): void;
+    readSync(): number[];
+    readTimeout(time_out: number): number[];
+    sendFeatureReport(data: number[]): number;
+    getFeatureReport(report_id: number, report_length: number): number[];
+    getDeviceInfo(): string;
+    resume(): void;
+    on(event: string, handler: (value: any) => void): void;
+    write(values: number[]): number;
+    setDriverType(type: string): void;
+  }
+  export function devices(): Device[];
 }
-export function devices(): Device[];
